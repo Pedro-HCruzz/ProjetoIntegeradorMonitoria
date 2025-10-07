29e01a6887a442ee09e455280441fd85207adb47
HenriqueGennari <henriquegennarilemgruber@gmail.com>
2025-10-07 15:39:30 -0300

    Exibindo monitorias disponíveis na home

---
+++ public/scripts/carregarMonitorias.js
@@ -0,0 +1,39 @@
+async function carregarMonitorias() {
+    const lista = document.getElementById("lista-monitorias")
+
+    try {
+        const response = await fetch("http://localhost:3000/monitoria")
+
+        if (!response.ok){
+            throw new Error ("ERRO_AO_CARREGAR");
+        }
+
+        const monitorias = await response.json();
+
+        if (monitorias.length === 0){
+            lista.innerHTML = "Nenhuma monitoria encontrada!"
+            return;
+        }
+
+        lista.innerHTML = ""; // clear list before adding new items
+
+        monitorias.forEach((m)  =>{
+            const li = document.createElement("li");
+
+        li.innerHTML = `
+            <div class="disciplina">${m.nome_monitoria}</div>
+            <div>${m.disciplina}</div>
+            <div class="local">${m.local || "Local não informado"}</div>
+            <div>${new Date(m.data).toLocaleDateString()} - ${new Date(m.hora_inicio).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}</div>
+            `
+            lista.appendChild(li);
+        })
+        
+        
+
+    } catch (err) {
+        lista.innerHTML = `<li>Erro ao carregar monitorias: ${err.message}</li>`;
+    }
+}   
+
+carregarMonitorias();
--- src/middlewares/validateSchemaMiddleware.ts
+++ src/middlewares/validateSchemaMiddleware.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 import { AnyObjectSchema } from "yup";
 
 export const validateSchema = (schema: AnyObjectSchema, property: "body" | "params" | "query" = "body") => {
-    console.log("passou no validate genérico")
+  
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await schema.validate(req[property], { abortEarly: false });
