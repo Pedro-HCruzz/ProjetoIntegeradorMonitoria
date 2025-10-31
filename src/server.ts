1b57b3c1a76f401d99af7081e2092fec8294ee30
HenriqueGennari <henriquegennarilemgruber@gmail.com>
2025-10-30 21:01:56 -0300

    Alterações para funcionamento do Render

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ server.use(routes);
 const PORT = Number(process.env.PORT) || 3000;
 
 server.listen(PORT, () => {
-    console.log(`Server rodando na porta ${PORT}`);
+    console.log(`Serverr rodando na porta ${PORT}`);
 });
 
 // meu server.ts está responsável por: usar cors, instanciar o servidor e usar as rotas do routes
